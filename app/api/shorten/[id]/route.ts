d4cf041926500880ba33b120a9b1ee70e9e9c104
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2024-12-04 21:08:00 -0300

    fix: adjusting findUnique in api routes

--- app/api/shorten/[id]/route.ts
+++ app/api/shorten/[id]/route.ts
@@ -1,33 +1,25 @@
-import { db } from '@/lib/db'
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function GET(
-  req: NextRequest,
-  { params }: { params: Promise<{ id: string }> },
-) {
-  return http.GET(req, { params })
+export async function GET() {
+  return http.GET()
 }
 
 const http = {
-  GET: async (
-    _: NextRequest,
-    { params }: { params: Promise<{ id: string }> },
-  ) => {
-    const id = (await params).id
+  GET: async () => {
+    // const id = (await params).id
 
-    if (id) {
-      const url = await db.url.findUnique({
-        where: { id: id.toString() },
-      })
+    // if (id) {
+    //   const url = await db.url.findUnique({
+    //     where: { id: id.toString() },
+    //   })
 
-      if (url) {
-        const updatedUrl = await db.url.update({
-          where: { id: url.id },
-          data: { visits: url.visits + 1 },
-        })
+    //   if (url) {
+    //     const updatedUrl = await db.url.update({
+    //       where: { id: url.id },
+    //       data: { visits: url.visits + 1 },
+    //     })
 
-        return NextResponse.json({ originalUrl: updatedUrl.originalUrl })
-      }
-    }
+    //     return NextResponse.json({ originalUrl: updatedUrl.originalUrl })
+    //   }
+    // }
+    return Response.json({ ok: true })
   },
 }
