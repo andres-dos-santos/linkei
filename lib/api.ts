749a3e26623b4ae20aa5e3d7ab8f0ea0aebacb8e
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2024-12-06 08:43:17 -0300

    fix: remove mongoose & api

--- lib/api.ts
+++
@@ -1,8 +0,0 @@
-export async function api(url: string, init?: RequestInit) {
-  const response = await fetch(process.env.NEXT_PUBLIC_BASE_URL + url, {
-    ...init,
-    headers: { 'Content-Type': 'application/json' },
-  })
-
-  return response
-}
