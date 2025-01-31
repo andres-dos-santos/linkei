6b63e69b195df89eb2dc97e9feebc672af571e02
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-01-31 13:13:40 -0300

    <SignIn /> card style

--- components/profile.tsx
+++ components/profile.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { setUser } from '@/app/actions'
-import { SignInButton, SignOutButton, useSession } from '@clerk/nextjs'
+import { SignIn, SignInButton, SignOutButton, useSession } from '@clerk/nextjs'
 import { useEffect } from 'react'
 
 export function Profile() {
@@ -15,16 +15,41 @@ export function Profile() {
 		set()
 	}, [session])
 
-	return !isSignedIn ? (
-		<SignInButton>
-			<div className="ml-3 mt-4 mb-2.5 flex items-center">
-				<p className="cursor-pointer text-[13px] font-medium text-zinc-600 -tracking-wide mt-2.5 dark:text-zinc-300">
-					Ainda não tem conta?{' '}
-					<span className="text-blue-500 underline">Clique aqui</span>.
-				</p>
+	if (!isSignedIn) {
+		return (
+			<div className="flex flex-col pt-5">
+				<span className="text-[13px] font-medium text-zinc-600 text-center -tracking-wide dark:text-zinc-300">
+					Quer salvar seus links e verificar <br /> a quantidade de cliques?
+				</span>
+				<SignIn
+					appearance={{
+						elements: {
+							headerTitle: 'hidden',
+							headerSubtitle: 'hidden',
+							card: 'bg-transparent rounded-none flex items-center justify-center pt-0 pb-6',
+							socialButtonsBlockButton: 'border-0 ring-0',
+							button: 'border-0 ring-0',
+							cardBox: 'bg-transparent rounded-none shadow-none',
+							footer: 'hidden',
+						},
+					}}
+				/>
 			</div>
-		</SignInButton>
-	) : (
+		)
+
+		// return (
+		// 	<SignInButton mode="modal">
+		// 		<div className="ml-3 mt-4 mb-2.5 flex items-center">
+		// 			<p className="cursor-pointer text-[13px] font-medium text-zinc-600 -tracking-wide mt-2.5 dark:text-zinc-300">
+		// 				Ainda não tem conta?{' '}
+		// 				<span className="text-blue-500 underline">Clique aqui</span>.
+		// 			</p>
+		// 		</div>
+		// 	</SignInButton>
+		// )
+	}
+
+	return (
 		<SignOutButton>
 			<div className="mr-auto ml-3 mt-4 mb-2.5 w-full flex cursor-pointer items-center">
 				<div className="h-8 w-8 bg-zinc-200 rounded-lg mr-2.5">
