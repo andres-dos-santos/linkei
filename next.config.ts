4a2aeb54b78e3e07ce0b399d02ecdca42d7ef7da
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-07-22 19:19:37 -0300

    fix(next.config.ts): comment code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	distDir: 'build',
+	/** distDir: 'build',
 	async headers() {
 		return [
 			{
@@ -21,7 +21,7 @@ const nextConfig: NextConfig = {
 				],
 			},
 		]
-	},
+	}, */
 }
 
 export default nextConfig
