b8a22a71b2ff560c6e59d01676476926006c4275
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-07-23 08:40:30 -0300

    feat(description): remove br tag from description

--- components/description.tsx
+++ components/description.tsx
@@ -1,7 +1,7 @@
 export function Description() {
 	return (
 		<p className="block my-10 text-sm lg:text-xs xl:text-sm font-geist text-zinc-500 dark:text-zinc-300 tracking-wide">
-			Vamos encurtar seu link e deixar ele pronto <br /> para compartilhar.
+			Vamos encurtar seu link e deixar ele pronto para compartilhar.
 		</p>
 	)
 }
