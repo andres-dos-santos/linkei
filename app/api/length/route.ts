442ebe495c9249cf3971974f9409ffc0a8a052b0
andres-dos-santos <andres.dosantosbritoamaral@gmail.com>
2025-09-01 18:33:37 -0300

    fix: adding database connection on router

--- app/api/length/route.ts
+++ app/api/length/route.ts
@@ -3,8 +3,11 @@
 import { type NextRequest, NextResponse } from 'next/server'
 
 import { URL } from '@/services/models/url'
+import { DBConnect } from '@/services/mongo'
 
 export const GET = async (req: NextRequest) => {
+	await DBConnect()
+
 	const length = await URL.countDocuments()
 
 	return NextResponse.json({ length })
